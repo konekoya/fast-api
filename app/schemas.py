9a9e0af1ebf4434aac11bf7c248d7c04b69993e3
Joshua <tank158r5155@yahoo.com.tw>
2023-02-14 11:22:33 +0800

    Add vote route

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 
 from app.database import Base
 
@@ -52,3 +52,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)  # type: ignore
